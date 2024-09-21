cbb7531f6489071892391617a952d7b4602393cf
Sunggook Chue <sunggch@microsoft.com>
2024-09-21 16:20:34 -0700

    setPreferredSinkId site

---
+++ iframe.js
@@ -0,0 +1,64 @@
+let mainDeviceId = null;
+let secondDeviceId = null;
+let selectedDeviceId = null;
+let output = null;
+
+function logMessage(msg, error) {
+  const errorElement = document.querySelector('#logMessage');
+  errorElement.innerHTML = `<p>${msg}</p>`;
+  if (typeof error !== 'undefined') {
+    console.log(error);
+  }
+}
+
+function handleSuccess(msg) {
+  logMessage(`Success: ${msg}`);
+}
+
+function handleError(msg) {
+  logMessage(`Error: ${msg}`);
+}
+
+function errorMsg(msg, error) {
+  const errorElement = document.querySelector('#errorMsg');
+  errorElement.innerHTML += `<p>${msg}</p>`;
+  if (typeof error !== 'undefined') {
+    console.log(error);
+  }
+}
+
+const defaultButton = document.getElementById('default1');
+defaultButton.addEventListener('click', () => {
+  selectedDeviceId = "default";
+  logMessage(`Selected DeviceId: ${selectedDeviceId}`);
+});
+
+const communicationsButton = document.getElementById('communications');
+communicationsButton.addEventListener('click', () => {
+  selectedDeviceId = "communications";
+  logMessage(`Selected DeviceId: ${selectedDeviceId}`);
+});
+
+const customButton = document.getElementById('custom');
+customButton.addEventListener('click', () => {
+  let customDeviceId = document.getElementById("customInput").value;
+  selectedDeviceId = customDeviceId;
+  logMessage(`Selected DeviceId: ${selectedDeviceId}`);
+});
+
+const defaultSinkIdButton = document.getElementById('setDefaultSinkId');
+defaultSinkIdButton.addEventListener('click', async () => {
+  try {
+    await navigator.mediaDevices.setPreferredSinkId(`${selectedDeviceId}`);
+  } catch (e) {
+    handleError(`setPreferredSinkId: ${selectedDeviceId}, ${e}`)
+  }
+});
+
+window.addEventListener('message', (event) => {
+  console.log('Message received in iframe:', event.data);
+  selectedDeviceId = parseInt(event.data, 10);
+  if (isNaN(selectedDeviceId)) {
+    selectedDeviceId = event.data;
+  }
+});
--- main.js
+++ main.js
@@ -1,36 +1,126 @@
-/*
- *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
 'use strict';
 
-const enumDeviceButton = document.getElementById('enumDevices');
+let firstDeviceid = "";
+let thirdDeviceId = "";
+let selectedDeviceId = "";
 
-function handleError(error) {
-  errorMsg(`getDisplayMedia error: ${error.name}`, error);
-}
+let mediaRecorder;
+let recordedBlobs;
 
-function log(msg, error) {
-  const errorElement = document.querySelector('#errorMsg');
-  errorElement.innerHTML += `${msg}<br>`;
-}
-
-enumDeviceButton.addEventListener('click', () => {
-  function getConnectedDevices(type, callback) {
-    navigator.mediaDevices.enumerateDevices()
-        .then(devices => {
-            devices.forEach((device) => {
-              log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
-            });
-          
-            // const filtered = devices.filter(device => device.kind === type);
-            // callback(filtered);
-        });
+function logMessage(msg, add) {
+  const log = document.querySelector('#logMessage');
+  if (add) {
+    log.innerHTML += `<p>${msg}</p>`;
+  } else {
+    log.innerHTML = `<p>${msg}</p>`;
   }
-  
-  getConnectedDevices('audiooutput', cameras => log('speaker found:' + cameras));
+}
+
+function handleSuccess(msg) {
+  logMessage(`Success: ${msg}`, true);
+}
+
+function handleError(msg) {
+  logMessage(`Error: ${msg}`, true);
+}
+
+const defaultButton = document.getElementById('default1');
+defaultButton.addEventListener('click', () => {
+  selectedDeviceId = "default";
+  logMessage(`Selected DeviceId: ${selectedDeviceId}`);
+  iframe1.contentWindow.postMessage(`${selectedDeviceId}`, '*');
+  iframe2.contentWindow.postMessage(`${selectedDeviceId}`, '*');  
+});
+
+const communicationsButton = document.getElementById('communications');
+communicationsButton.addEventListener('click', () => {
+  selectedDeviceId = "communications";
+  logMessage(`Selected DeviceId: ${selectedDeviceId}`);
+  iframe1.contentWindow.postMessage(`${selectedDeviceId}`, '*');
+  iframe2.contentWindow.postMessage(`${selectedDeviceId}`, '*');  
+});
+
+const customButton = document.getElementById('custom');
+customButton.addEventListener('click', () => {
+  let customDeviceId = document.getElementById("customInput").value;
+  selectedDeviceId = customDeviceId;
+  logMessage(`Selected DeviceId: ${selectedDeviceId}`);
+
+  iframe1.contentWindow.postMessage(`${selectedDeviceId}`, '*');
+  iframe2.contentWindow.postMessage(`${selectedDeviceId}`, '*');
+});
+
+const defaultSinkIdButton = document.getElementById('setDefaultSinkId');
+defaultSinkIdButton.addEventListener('click', async () => {
+  try {
+    await navigator.mediaDevices.setPreferredSinkId(`${selectedDeviceId}`);
+  } catch (e) {
+    handleError(`setPreferredSinkId: ${selectedDeviceId}, ${e}`)
+  }
+});
+
+// let audioCtx = null;
+// const audioContextButton = document.getElementById('setSinkId_audioContext');
+// audioContextButton.addEventListener('click', () => {
+//   const videoElem = document.getElementById('video1');
+//     // Create an AudioContext
+//     if (!audioCtx) {
+//       audioCtx = new AudioContext();
+//     }
+
+//     console.log(`init audioContext sinkId: ${audioCtx.sinkId}`);
+
+//     let audioDefaultDeviceId = selectedDeviceId === "default" ? "" : selectedDeviceId;
+//     console.log(`set audioContext sinkId with: ${audioDefaultDeviceId}`);
+
+//     audioCtx.setSinkId(audioDefaultDeviceId)
+//     .then(handleSuccess(`audioContext: ${audioDefaultDeviceId}`))
+//     .catch(err => handleError(`audioContext: ${err}`));
+
+//     // Create an oscillator node (generates sound)
+//     const oscillatorNode = audioCtx.createOscillator();
+
+//     // Create a gain node (controls volume)
+//     const gainNode = audioCtx.createGain();
+
+//     // Connect the oscillator to the gain node
+//     oscillatorNode.connect(gainNode);
+
+//     // Connect the gain node to the audio output (speakers)
+//     gainNode.connect(audioCtx.destination);
+
+//     // Set oscillator properties (e.g., frequency)
+//     oscillatorNode.type = 'sine'; // Sine wave
+//     oscillatorNode.frequency.value = 440; // Frequency in Hz
+
+//     // Set gain properties (e.g., volume)
+//     gainNode.gain.value = 0.8; // Volume (0 to 1)
+
+//     // Start the oscillator
+//     oscillatorNode.start();
+
+//     // Stop the oscillator after 0.5 seconds
+//     oscillatorNode.stop(audioCtx.currentTime + 0.5);
+// });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  navigator.mediaDevices.enumerateDevices()
+    .then((devices) => {
+      const audioCtx = null;
+      let order = 0;
+      devices.forEach((device) => {
+        if (device.kind === 'audiooutput') {
+          logMessage(`${device.label}, ${device.deviceId}`, true);
+          ++order;
+          if (order == 1)
+            firstDeviceid = device.deviceId;
+
+          if (device.label.indexOf('Communications') !== -1) {
+            thirdDeviceId = device.deviceId;
+            return;
+          }
+        }
+      });
+    }).catch(handleError);
 });
 
